6909d9817a7747533a6e40bbe118a4e82fd759f3
Russell Parrish <russ@Russells-Mac-mini.local>
2024-03-15 19:15:47 -0400

    home template

--- rocketrycloud/urls.py
+++ rocketrycloud/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rocketrycloud import views
 
 urlpatterns = [
+    path('', views.index),
     path("clubs/", include("clubs.urls")),
     path('admin/', admin.site.urls),
 ]
